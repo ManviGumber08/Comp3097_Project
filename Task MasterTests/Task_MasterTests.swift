c6156b563fa0159ad33e74b76e20c4d66e132758
Manvi Gumber <101412099@georgebrown.ca>
2025-02-15 18:06:49 -0500

    Initial Commit

---
+++ Task MasterTests/Task_MasterTests.swift
@@ -0,0 +1,17 @@
+//
+//  Task_MasterTests.swift
+//  Task MasterTests
+//
+//  Created by Manvi Gumber on 2025-02-15.
+//
+
+import Testing
+@testable import Task_Master
+
+struct Task_MasterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
